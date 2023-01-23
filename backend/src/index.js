05f6aa8c3f37049fbece615056c6e64d0654952d
dani54196 <dani54196@gmail.com>
2023-01-23 17:18:54 -0300

    docker & db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import express from "express";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
+import("./models/index.models.js");
 import indexRoutes from "./routes/index.routes.js";
 
 // config
---
+++ backend/src/models/index.models.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize";
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize("menu", "root", "pass", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+export default sequelize;
