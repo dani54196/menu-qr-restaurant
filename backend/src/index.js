d4bb7237124ab303032706367dbecf347c970b6e
dani54196 <dani54196@gmail.com>
2023-01-23 18:29:45 -0300

    ignore DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import express from "express";
 
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from "dotenv";
 dotenv.config();
 
 import("./models/index.models.js");
