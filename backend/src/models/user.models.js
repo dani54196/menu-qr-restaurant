aef0c804770b3ca6a2acff70ef205d18f9f924c1
dani54196 <dani54196@gmail.com>
2023-02-06 09:52:25 -0300

    brycpt

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -8,28 +8,39 @@ User.init(
   {
     user_id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 100],
+        isPassword: function (value) {
+          // Check for special characters
+          if (!/[!@#$%^&*]/.test(value)) {
+            throw new Error("Password must contain a special character");
+          }
+        },
+      },
+    },
     firstName: { type: DataTypes.STRING },
     lastName: { type: DataTypes.STRING },
   },
   {
-    hooks: {
-      beforeCreate: (user) => {
-        user.password = bcrypt.hashSync(user.password, 10);
-      },
-      beforeUpdate: (user) => {
-        user.password = bcrypt.hashSync(user.password, 10);
-      },
-    },
-    // instanceMethods:{
-    //   validPassword: function(password) {
-    //     return bcrypt.compareSync(password, user.password);
-    //   }
-    // },
     timestamps: true,
     sequelize, // We need to pass the connection instance
     modelName: "User", // We need to choose the model name
   }
 );
+
+User.beforeCreate(async (user) => {
+  user.password = await bcrypt.hash(user.password, 10);
+});
+
+User.beforeUpdate(async (user) => {
+  user.password = await bcrypt.hash(user.password, 10);
+});
+
+User.prototype.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export default User;
